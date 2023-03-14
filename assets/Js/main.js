62a032a5226da11d9f0874b74b3520a2ea1a9cfa
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-14 16:37:32 +0100

    ciclo for

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -5,20 +5,25 @@
 /* strumenti: 
 
 */
-
-
-//creiamo un array con i link delle immagini
-const images = [
-"./assets/img/01.webp"
-"./assets/img/02.webp"
-"./assets/img/03.webp"
-"./assets/img/04.webp"
-"./assets/img/05.webp"
-]
+//creiamo un array con i link delle immagini 
+const carosello = [
+    '"./assets/img/01.webp"',
+    '"./assets/img/02.webp"',
+    '"./assets/img/03.webp"',
+    '"./assets/img/04.webp"',
+    '"./assets/img/05.webp"'
+    ]
+// 
+const containerImg = document.querySelector('.carosello')
+const imgNext = document-querySelector('.next')
+const imgPrev = document-querySelector('.prev')
 
 //contatore 
 let activeImage = 0
 
-const imagesElement = document.querySelector('.carosello > .images')
+const caroselloElement = document.querySelector('.carosello')
 
-for ()
+for (let i = 0; i < carosello.length; i++){
+    const imgSrc = carosello[i]
+
+}
