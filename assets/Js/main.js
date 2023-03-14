3e38276e90b06948ae00813dccd25579cc766f32
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-14 16:08:27 +0100

    array for images

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -5,3 +5,20 @@
 /* strumenti: 
 
 */
+
+
+//creiamo un array con i link delle immagini
+const images = [
+"./assets/img/01.webp"
+"./assets/img/02.webp"
+"./assets/img/03.webp"
+"./assets/img/04.webp"
+"./assets/img/05.webp"
+]
+
+//contatore 
+let activeImage = 0
+
+const imagesElement = document.querySelector('.carosello > .images')
+
+for ()
